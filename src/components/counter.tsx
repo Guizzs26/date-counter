7f8e61808f0a39cf538c02b7515471116567b75d
Guilherme <guiteixeira1926@gmail.com>
2024-07-10 18:57:26 -0400

    feat: add all styles

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -26,11 +26,16 @@ export function Counter() {
   };
 
   const handleCountChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setCount(Number(e.target.value));
+    const value = Number(e.target.value);
+    if (!isNaN(value)) {
+      setCount(value);
+    }
   };
 
   return (
     <>
+      <h1 className={styles.title}>Date Counter</h1>
+
       <div className={styles.inputBox}>
         <input
           type="range"
@@ -38,31 +43,47 @@ export function Counter() {
           max={10}
           value={step}
           onChange={handleStepChange}
+          className={styles.inputBoxRange}
         />
-        <span>{step}</span>
+        <span className={styles.inputBoxSpan}>{step}</span>
       </div>
 
       <div className={styles.inputBox}>
-        <button onClick={handleDecrementCounter}>-</button>
-        <input type="text" value={count} onChange={handleCountChange} />
-        <button onClick={handleIncrementCounter}>+</button>
+        <button
+          onClick={handleDecrementCounter}
+          className={styles.inputBoxButton}
+        >
+          -
+        </button>
+        <input
+          type="text"
+          value={count}
+          onChange={handleCountChange}
+          className={styles.inputBoxInput}
+        />
+        <button
+          onClick={handleIncrementCounter}
+          className={styles.inputBoxButton}
+        >
+          +
+        </button>
       </div>
 
-      {
-        <div>
-          <span>
-            {count === 0
-              ? "Today is "
-              : count > 0
-              ? `${count} ${count === 1 ? "day" : "days"} from today is `
-              : `${Math.abs(count)} ${count === -1 ? "day" : "days"} ago was `}
-          </span>
-          <span>{date.toDateString()}</span>
-        </div>
-      }
+      <div className={styles.dateContainer}>
+        <span className={styles.dateText}>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} ${count === 1 ? "day" : "days"} from today is `
+            : `${Math.abs(count)} ${count === -1 ? "day" : "days"} ago was `}
+        </span>
+        <span className={styles.dateValue}>{date.toDateString()}</span>
+      </div>
 
       {step !== 1 || count !== 0 ? (
-        <button onClick={handleReset}>Reset</button>
+        <button onClick={handleReset} className={styles.resetButton}>
+          Reset
+        </button>
       ) : null}
     </>
   );
